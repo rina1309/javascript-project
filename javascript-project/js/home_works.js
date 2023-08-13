f594346730b5887367b19e5fb07d3a7d311334b4
rina1309 <113968192+rina1309@users.noreply.github.com>
2023-08-14 00:48:26 +0600

    Add files via upload
    DZ 2

--- javascript-project/js/home_works.js
+++ javascript-project/js/home_works.js
@@ -18,37 +18,69 @@ gmailButton.onclick = () => {
 
 //move block
 
-const parentBlock = document.querySelector(".parent_block");
-const childBlock = document.querySelector(".child_block");
+const childBlock = document.querySelector(".child_block")
 
-const moveRight = (position) => {
-    if (position >= 90) {
-        moveLeft(90);
-        return;
-    }
+let positionLeft = 0
+let positionRight = 0
+let positionBottomRight = 0
+let positionBottomLeft = 0
 
-    childBlock.style.left = position + "%";
-
-    setTimeout(() => {
-        moveRight(position + 1);
-    }, 10);
-}
-const moveLeft = (position) => {
-    if (position <= 0) {
-        return;
-    }
-
-    childBlock.style.left = position + "%";
-
-    setTimeout(() => {
-        moveLeft(position - 1);
-    }, 10);
-}
 const moveBlock = () => {
-    if (!childBlock.style.left || childBlock.style.left === "0%") {
-        moveRight(0);
-    } else if (childBlock.style.left === "90%") {
-        moveLeft(90);
+    if (positionLeft === 0) {
+        positionLeft++
+        childBlock.style.right = positionLeft + "px"
+        setTimeout(moveBlock, 1500)
+    } else if (positionRight === 0 && positionBottomRight === 0) {
+        positionRight++
+        childBlock.style.bottom = positionRight + "px"
+        setTimeout(moveBlock, 1500)
+    } else if (positionBottomRight === 0 && positionBottomLeft < 448) {
+        positionBottomRight++
+        childBlock.style.left = positionBottomRight + "px"
+        setTimeout(moveBlock, 1500)
+    } else if (positionBottomLeft === 0 && positionLeft < 448) {
+        positionBottomLeft++
+        childBlock.style.top = positionBottomLeft + "px"
+        setTimeout(moveBlock, 1500)
     }
 }
-moveBlock();
+moveBlock()
+
+//Stop Watch
+
+const timer = document.querySelector('#seconds')
+const startButton = document.querySelector('#start')
+const stopButton = document.querySelector('#stop')
+const resetButton = document.querySelector('#reset')
+const resumeButton = document.querySelector('#resume')
+
+let num = 0
+let resetTimer = null
+
+const updateTimer = () => {
+    num++
+    timer.innerText = num
+}
+startButton.onclick = () => {
+    if (!resetTimer) {
+        resetTimer = setInterval(updateTimer, 1000)
+    }
+}
+resetButton.onclick = () => {
+    clearInterval(resetTimer)
+    resetTimer = null
+    num = 0
+    timer.innerText = num
+}
+stopButton.onclick = () => {
+    clearInterval(resetTimer)
+    resetTimer = null
+}
+resumeButton.onclick = () => {
+    if (!resetTimer) {
+        resetTimer = setInterval(updateTimer, 1000)
+    }
+}
+
+
+
